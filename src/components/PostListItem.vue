6f4fc094d11be3a5162a1022e6d1015212d5d96c
justdheja <justdheja@gmail.com>
2020-06-01 23:09:25 +0700

    Forum Component

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -17,7 +17,7 @@
         </div>
 
         <div class="post-date text-faded">
-          <AppDate :timestamp="post.publishAt"/>
+          <AppDate :timestamp="post.publishedAt"/>
         </div>
       </div>
 </template>
