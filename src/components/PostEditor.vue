3b2ba471432ad6c509d12c4f16955b4aed6db5f0
justdheja <justdheja@gmail.com>
2020-06-01 18:54:07 +0700

    human friendly dates

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea 
+          name="" 
+          id="" 
+          cols="30" 
+          rows="10"
+          class="form-input"
+          :value="text"
+          @input="text = $event.target.value"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">
+          Submit Post
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    threadId: {
+      required: true,
+
+    }
+  },
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.text,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.threadId,
+          userId: 'L664y3qZSubDbT1R6npC0EEybJ73',
+          '.key': postId
+        }
+        
+        this.text = ''
+
+        this.$emit('save', {post})
+      }
+  },
+}
+</script>
