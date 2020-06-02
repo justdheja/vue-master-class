6b1d58a14d375beaa5d988f1575d5934ae9d0ea4
justdheja <justdheja@gmail.com>
2020-06-02 10:55:40 +0700

    migrating to vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import AppDate from '@/components/AppDate'
+import store from './store/index'
 
 Vue.component('AppDate', AppDate)
 
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
