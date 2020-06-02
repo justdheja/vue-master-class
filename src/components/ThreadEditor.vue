4cda8735c1f01cad64e45832332405fd751258ec
justdheja <justdheja@gmail.com>
2020-06-02 21:24:40 +0700

    improve thread editor

--- src/components/ThreadEditor.vue
+++ src/components/ThreadEditor.vue
@@ -1,40 +1,57 @@
 <template>
-  <div>
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input v-model="title" type="text" id="thread_title" class="form-input" name="title">
-      </div>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input v-model="form.title" type="text" id="thread_title" class="form-input" name="title">
+    </div>
 
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea v-model="text" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
-      </div>
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea v-model="form.text" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
+    </div>
 
-      <div class="btn-group">
-        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-      </div>
-    </form>
-  </div>
+    <div class="btn-group">
+      <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
+      <button class="btn btn-blue" type="submit" name="Publish">{{isUpdate ? 'Update' : 'Publish'}} </button>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: '',
-      text: ''
+    export default {
+      props: {
+        title: {
+          type: String,
+          default: ''
+        },
+        text: {
+          type: String,
+          default: ''
+        }
+      },
+      data () {
+        return {
+          form: {
+            title: this.title,
+            text: this.text
+          }
+        }
+      },
+      computed: {
+        isUpdate() {
+          return !!this.title
+        }
+      },
+      methods: {
+        save () {
+          this.$emit('save', {title: this.form.title, text: this.form.text})
+        },
+        cancel () {
+          this.$emit('cancel')
+        }
+      }
     }
-  },
-
-  methods: {
-    save() {
-      this.$emit('save', {title: this.title, text: this.text})
-    },
-    cancel() {
-      this.$emit('cancel')
-    }
-  },
-}
 </script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
-import Category from '@/pages/PageCategory'
 import ThreadShow from '@/pages/PageThreadShow'
 import ThreadCreate from '@/pages/PageThreadCreate'
-import NotFound from '@/pages/PageNotFound'
+import ThreadEdit from '@/pages/PageThreadEdit'
+import Category from '@/pages/PageCategory'
 import Forum from '@/pages/PageForum'
 import Profile from '@/pages/PageProfile'
-
+import NotFound from '@/pages/PageNotFound'
 Vue.use(Router)
 
 export default new Router({
@@ -30,18 +30,6 @@ export default new Router({
       props: true
     },
     {
-      path: '/me',
-      name: 'Profile',
-      component: Profile,
-      props: true
-    },
-    {
-      path: '/me/edit',
-      name: 'ProfileEdit',
-      component: Profile,
-      props: {edit: true}
-    },
-    {
       path: '/thread/create/:forumId',
       name: 'ThreadCreate',
       component: ThreadCreate,
@@ -52,6 +40,24 @@ export default new Router({
       name: 'ThreadShow',
       component: ThreadShow,
       props: true
+    },
+    {
+      path: '/thread/:id/edit',
+      name: 'ThreadEdit',
+      component: ThreadEdit,
+      props: true
+    },
+    {
+      path: '/me',
+      name: 'Profile',
+      component: Profile,
+      props: true
+    },
+    {
+      path: '/me/edit',
+      name: 'ProfileEdit',
+      component: Profile,
+      props: {edit: true}
     },
     {
       path: '*',
