3dfa60d55801ce84223a0c5148dc4e2217950cbe
justdheja <justdheja@gmail.com>
2020-06-02 22:26:44 +0700

    post editor buttons

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -1,51 +1,73 @@
 <template>
-  <div>
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <textarea 
-          name="" 
-          id="" 
-          cols="30" 
+  <form @submit.prevent="save">
+    <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
           rows="10"
           class="form-input"
-          :value="text"
-          @input="text = $event.target.value"
+          v-model="text"
         ></textarea>
-      </div>
-      <div class="form-actions">
-        <button class="btn-blue">
-          Submit Post
-        </button>
-      </div>
-    </form>
-  </div>
+    </div>
+    <div class="form-actions">
+      <button v-if="isUpdate" @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
+      <button class="btn-blue">{{isUpdate ? 'Update' : 'Submit Post'}}</button>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
-  props: {
-    threadId: {
-      required: true,
-
-    }
-  },
-  data() {
-    return {
-      text: ''
-    }
-  },
-  methods: {
-    save () {
-        const post = {
-          text: this.text,
-          threadId: this.threadId,
+    export default {
+      props: {
+        threadId: {
+          required: false
+        },
+        post: {
+          type: Object
         }
-        
-        this.text = ''
-
-        this.$emit('save', {post})
-        this.$store.dispatch('createPost', post)
+      },
+      data () {
+        return {
+          text: this.post ? this.post.text : ''
+        }
+      },
+      computed: {
+        isUpdate () {
+          return !!this.post
+        }
+      },
+      methods: {
+        save () {
+          this.persist()
+            .then(post => {
+              this.$emit('save', {post})
+            })
+        },
+        cancel() {
+          this.$emit('cancel')
+        },
+        create () {
+          const post = {
+            text: this.text,
+            threadId: this.threadId
+          }
+          this.text = ''
+          return this.$store.dispatch('createPost', post)
+        },
+        update () {
+          const payload = {
+            id: this.post['.key'],
+            text: this.text
+          }
+          return this.$store.dispatch('updatePost', payload)
+        },
+        persist () {
+          return this.isUpdate ? this.update() : this.create()
+        }
       }
-  },
-}
+    }
 </script>
+
+<style scoped>
+</style>
--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -1,45 +1,66 @@
 <template>
   <div class="post">
-        <div class="user-info">
-          <a href="#" class="user-name">{{user.name}}</a>
+    <div class="user-info">
+      <a href="#" class="user-name">{{user.name}}</a>
 
-          <a href="#">
-            <img class="avatar-large" :src="user.avatar" alt="">
-          </a>
+      <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt="">
+      </a>
 
-          <p class="desktop-only text-small">{{userPostCount}} posts</p>
+      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
+    </div>
+
+    <div class="post-content">
+      <template v-if="!editing">
+        <div>
+          {{post.text}}
         </div>
-
-        <div class="post-content">
-          <div>
-            {{post.text}}
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          <AppDate :timestamp="post.publishedAt"/>
-        </div>
+        <a @click.prevent="editing = true" href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
+      </template>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+          @cancel="editing = false"
+        />
       </div>
+
+    </div>
+
+    <div class="post-date text-faded">
+      <AppDate :timestamp="post.publishedAt"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import {countObjectProperties} from '../utils/index'
-
-export default {
-  props:{
-    post: {
-      required: true,
-      type: Object
+    import {countObjectProperties} from '@/utils'
+    import PostEditor from './PostEditor'
+    export default {
+      props: {
+        post: {
+          required: true,
+          type: Object
+        }
+      },
+      components: {
+        PostEditor
+      },
+      data () {
+        return {
+          editing: false
+        }
+      },
+      computed: {
+        user () {
+          return this.$store.state.users[this.post.userId]
+        },
+        userPostsCount () {
+          return countObjectProperties(this.user.posts)
+        }
+      }
     }
-  },
-  computed:{
-    user () {
-      return this.$store.state.users[this.post.userId]
-    },
-    userPostCount () {
-      return countObjectProperties(this.user.posts)
-    },
-  },
-  
-}
 </script>
+
+<style scoped>
+</style>
