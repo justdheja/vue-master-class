6896132030da81b874ba08626609484ce5117fc8
justdheja <justdheja@gmail.com>
2020-06-03 17:38:38 +0700

    install firebase rtdb

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import firebase from 'firebase'
 import router from './router'
 import AppDate from '@/components/AppDate'
 import store from './store/index'
@@ -10,6 +11,19 @@ Vue.component('AppDate', AppDate)
 
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}
+
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
