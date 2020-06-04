bd9cbe2683ba3a2e65e16b4381a24c39f53fea85
justdheja <justdheja@gmail.com>
2020-06-04 09:52:57 +0700

    spliting vuex & add threads to db

--- src/main.js
+++ src/main.js
@@ -31,5 +31,8 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  beforeCreate() {
+    store.dispatch('fetchUser', {id: store.state.authId})
+  },
 })
