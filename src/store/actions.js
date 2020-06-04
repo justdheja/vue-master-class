2b7b93016f4d38b9aa6c4b150d4bdff6a21e03e3
justdheja <justdheja@gmail.com>
2020-06-04 10:04:45 +0700

    counting thread & update firebase

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,12 +8,14 @@ export default {
 
       const updates = {}
       updates[`posts/${postId}`] = post
-      updates[`threads/${post.threadId}/posts/${postId}`] = postId 
-      updates[`users/${post.userId}/posts/${postId}`] = postId 
+      updates[`threads/${post.threadId}/posts/${postId}`] = postId
+      updates[`threads/${post.threadId}/contributors/${post.userId}`] = post.userId
+      updates[`users/${post.userId}/posts/${postId}`] = postId
       firebase.database().ref().update(updates)
         .then(() => {
           commit('setItem', {resource: 'posts', item: post, id: postId})
           commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+          commit('appendContributorToThread', {parentId: post.threadId, childId: post.userId})
           commit('appendPostToUser', {parentId: post.userId, childId: postId})
           return Promise.resolve(state.posts[postId])
         })
@@ -70,18 +72,17 @@ export default {
     updatePost ({state, commit}, {id, text}) {
       return new Promise((resolve, reject) => {
         const post = state.posts[id]
-        commit('setPost', {
-          postId: id,
-          post: {
-            ...post,
-            text,
-            edited: {
-              at: Math.floor(Date.now() / 1000),
-              by: state.authId
-            }
-          }
-        })
-        resolve(post)
+        const edited = {
+          at: Math.floor(Date.now() / 1000),
+          by: state.authId
+        }
+  
+        const updates = {text, edited}
+        firebase.database().ref('posts').child(id).update(updates)
+          .then(() => {
+            commit('setPost', {postId: id, post: {...post, text, edited}})
+            resolve(post)
+          })
       })
     },
 
